ec5d6090210cc269a8bec3119145f8bfe3717365
Henriqueba2 <henrique.sbastos02@gmail.com>
2024-04-12 14:10:28 -0300

    firebase

---
+++ atividade.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAKfJTiCcyGurmHhCtfRd5MCwIhOvSVZI4",
+    authDomain: "atividade-1b4b6.firebaseapp.com",
+    projectId: "atividade-1b4b6",
+    storageBucket: "atividade-1b4b6.appspot.com",
+    messagingSenderId: "525472192590",
+    appId: "1:525472192590:web:b2c3cf4cc744bade868b1f",
+    measurementId: "G-02VL64LXV5"
+  };
+  
